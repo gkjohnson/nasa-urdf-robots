ed6b88018666712cbf688aa1ad2f2ed178a75f65
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-06-03 11:33:48 -0700

    Add a helper bash script to converting the files

---
+++ convert-xacro.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Provide <source> directory and then <target> directory as arguments"
+    exit 1
+fi
+
+# case insensitive file iteration
+shopt -s nocaseglob
+
+# iterate over the xacro files in the directory
+# and convert them to URDF files
+for filepath in $1/*.xacro; do
+
+    fullname=$(basename $filepath)
+    name=${fullname%.*}
+    targetpath=$2/$fullname.urdf
+
+    echo $filepath ">" $targetpath
+
+    rosrun xacro xacro echo -o "$filepath" "$targetpath"
+
+done
